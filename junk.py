d1cba3defd917ba352909e45fde231035811ca10
StefanStrang <stefan.strang@web.de>
2018-04-18 15:21:12 +0200

    stefan 18.04 15.39

---
+++ junk.py
@@ -0,0 +1,7 @@
+"""
+Authors: Julia Abels and Stefan Strang
+         University of Freiburg - 2018
+"""
+
+
+def polToJunk()
---
+++ lf.py
@@ -0,0 +1,9 @@
+"""
+Authors: Julia Abels and Stefan Strang
+         University of Freiburg - 2018
+"""
+
+
+
+def lf(formula, lfset):
+    pass
