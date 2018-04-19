496e8096f9ba4c2a180830ebebf7207a10aabdcc
StefanStrang <stefan.strang@web.de>
2018-04-19 09:18:33 +0200

    stefan main

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 """
 Authors: Julia Abels and Stefan Strang
          University of Freiburg - 2018
+
+editor notes:
+
+toGraphViz is now the correct function to print graph
 """
 
 import sys
 from ltlToPred import translate
 from getInp import getInp
-from toGraph import toGraph
+from toGraphViz import toGraph
 
 if __name__ == "__main__":
     #inp = getInp()
     #print(inp)
     #print(translate(inp))
-    toGraph(["A", "B", "C"], [["A","B"],["B","C"]])
+    #toGraph(["A", "B", "C"], [["A","B"],["B","C"]])
 
