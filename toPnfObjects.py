f77f288242f8b84d21c4127d42277ef66ddc75ff
StefanStrang <stefan.strang@web.de>
2018-05-04 16:16:13 +0200

    finished the pnf

--- toPnfObjects.py
+++ toPnfObjects.py
@@ -10,6 +10,7 @@ single = "XFG!"
 
 duo = "UWRMV&|"
 
+"""Class to init objects which are part of the L-Formula"""
 class lFormula:
 
     def __init__(self, name):
@@ -19,14 +20,14 @@ class lFormula:
         self.Atom = None
         self.Neg = False
 
-    def setName(self, name):
-        self.name = name
+    def setName(self, nombre):
+        self.name = nombre
 
     def setAtom(self):
         self.Atom = True
 
     def setNeg(self):
-        if (self.Neg == False):
+        if (self.Neg is False):
             self.Neg = True
         else:
             self.Neg = False
@@ -36,8 +37,6 @@ class lFormula:
 
     def setSec(self, objekt):
         self.pointSec = objekt
-
-
 
     def getName(self):
         return self.name
@@ -56,6 +55,7 @@ class lFormula:
 
 
 def toObjects(inPut):
+    """Iterate over the string and make them to objects."""
     inp = inPut.split()
     listedFormula = deepcopy(inp)
     lObjects = []
@@ -66,66 +66,118 @@ def toObjects(inPut):
     # lObjects.reverse()
     listedFormula.reverse()
     inp = listedFormula
-    inpObjects = lObjects
+    # inpObjects = lObjects
     """going over to objects manipulating them."""
     first = False
-    sec = False
+    # sec = False
     for i in range(len(lObjects)):
         if not (lObjects[i].getName() in duo or lObjects[i].getName() in single):
             """setting the Atom attribute. so no pointer goes out"""
             lObjects[i].setAtom()
-            sec = first
-            first = lObjects[i -1 ]
+            # sec = first
+            first = lObjects[i - 1]
         else:
             """if object is no item them set pointers
-            Diese Stelle ist schlampig gecoded und könnte fehlerpotential haben"""
+            Diese Stelle ist schlampig gecoded und
+            koennte fehlerpotential haben"""
             if(lObjects[i].getName() in single):
                 lObjects[i].setFirst(lObjects[i-1])
-                first = lObjects[i] # vllt raus oder anders
-                
+                first = lObjects[i]
             if(lObjects[i].getName() in duo):
                 lObjects[i].setFirst(lObjects[i-1])
                 lObjects[i].setSec(first)
                 first = lObjects[i]
     return lObjects, first
 
-def redQM(lObjects, ele):
-    if (ele.getFirst().getName() == "!"):
-       ele.setFirst(ele.getFirst().getFirst())
-       ele.getFirst().setNeg()
-       lObjects
 
-def dealQM(lObjects, first):
+def redEM(lObjects, ele):
+    """Eleminate "!" which are still objects. put them als negation
+    to the following object"""
+    if (ele.getFirst() is not None):
+        if (ele.getFirst().getName() == "!"):
+            lObjects.remove(ele.getFirst())
+            ele.setFirst(ele.getFirst().getFirst())
+            ele.getFirst().setNeg()
+        lObjects = redEM(lObjects, ele.getFirst())
+    if (ele.getSec() is not None):
+        if (ele.getSec().getName() == "!"):
+            lObjects.remove(ele.getSec())
+            ele.setSec(ele.getSec().getFirst())
+            ele.getSec().setNeg()
+        lObjects = redEM(lObjects, ele.getSec())
+    return lObjects
+
+
+def dealEM(lObjects, first):
+    """Boundary condition for elemination of exclamation marks.
+    also a helper for the recursion of redEM()"""
     if (first.getName() == "!"):
         lObjects.remove(first)
         first = first.getFirst()
         first.setNeg()
-    # print(lObjects)
-    redQM(lObjects, first)
-    print(first.getNeg())
-    print(first.getName())
-    prev = first.getFirst()
-    if (prev != None):
-        print(prev.getName())
-    prevprev = first.getSec()
-    if (prevprev != None):
-        print(prevprev.getName())
+    lObjects = redEM(lObjects, first)
+    return lObjects, first
 
-def pushIn(lObjects, first):
-    dealQM(lObjects, first)
-    # print(lObjects)
-    """print(first.getName())
-    for i in lObjects:
-        print("---")
-        print(i.getName())
-        prev = i.getFirst()
-        if (prev != None):
-            print(prev.getName())
-        prevprev = i.getSec()
-        if (prevprev != None):
-            print(prevprev.getName())"""
+
+def dealXFG(ele):
+    """before we can push in to the atoms. which is required for 
+    the pnf we need to transform not defined operators - XFG"""
+    # next: 		Xf - () - still to do - not in paper
+    if(ele.getName() == "F"):
+        ele.setName("U")
+        ele.setSec(ele.getFirst())
+        tt = lFormula("tt")
+        ele.setFirst(tt)
+        ele.getFirst().setAtom()
+    elif(ele.getName() == "G"):
+        ele.setName("R")
+        ele.setSec(ele.getFirst())
+        ff = lFormula("ff")
+        ele.setFirst(ff)
+        ele.getFirst().setAtom()
+    if(ele.getFirst() is not None):
+        dealXFG(ele.getFirst())
+    if(ele.getSec() is not None):
+        dealXFG(ele.getSec())
+
+
+def printdealXFG(ele):
+    """just a printer for debugging"""
+    print(ele.getName())
+    if(ele.getFirst() is not None):
+        printdealXFG(ele.getFirst())
+    if(ele.getSec() is not None):
+        printdealXFG(ele.getSec())
+
+
+def pushIn(ele):
+    """recursive in-pushing of the negation until atoms are reached"""
+    """here not sure whether logic is correct derived"""
+    if (ele.getNeg() is True):
+        if(ele.getName() == "U"):
+            ele.setName("R")
+        elif(ele.getName() == "R"):
+            ele.setName("U")
+        elif(ele.getName() == "&"):
+            ele.setName("|")
+        elif(ele.getName() == "|"):
+            ele.setName("&")
+        ele.setNeg()
+        if(ele.getFirst()is not None):
+            ele.getFirst().setNeg()
+        if(ele.getFirst()is not None):
+            ele.getSec().setNeg()
+    if(ele.getFirst() is not None):
+        pushIn(ele.getFirst())
+    if(ele.getSec() is not None):
+        pushIn(ele.getSec())
+
 
 def toPnf(inPut):
+    """Head-function to get formula in positive normal form"""
     lObjects, first = toObjects(inPut)
-    pushIn(lObjects, first)
-
+    lObjects, first = dealEM(lObjects, first)
+    dealXFG(first)
+    printdealXFG(first)
+    pushIn(first)
+    return first
